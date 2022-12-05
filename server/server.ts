1191cfab115084240fab6e4026fd39f513e0df0a
moyon_e <moyon.enzo@icloud.com>
2022-12-05 15:03:34 +0100

    [ADD] Express initialisation & .gitignore file for server

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,12 @@
 // API SERVER FILE
+
+import express, { Application, Request, Response } from 'express';
+
+const port = 5000;
+const app: Application = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+export default app;
