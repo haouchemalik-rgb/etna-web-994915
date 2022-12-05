0b6a5423bde30e58ebde9641e70417aedc7dbf6e
moyon_e <moyon.enzo@icloud.com>
2022-12-05 15:08:24 +0100

    [FIX] nodemon running

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,11 +2,15 @@
 
 import express, { Application, Request, Response } from 'express';
 
-const port = 5000;
+const port = 2000;
 const app: Application = express();
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Hello World!');
 });
+
+app.listen(port, () => {
+    console.log(`Your API is now listen on port ${port}`);
+  });  
 
 export default app;
