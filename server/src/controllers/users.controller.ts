a4f0d3ac1ae31c963e2c0f2ae40ac187f219ee34
montei_j <montei_j@etna-alternance.net>
2022-12-07 11:56:13 +0100

    [ADD] CRUD for USERS in services and controllers

--- server/src/controllers/users.controller.ts
+++ server/src/controllers/users.controller.ts
@@ -1,14 +1,72 @@
 import { Request, Response } from 'express';
-import { getByIdUsers } from '../services/users.service';
+import { 
+  getByIdUsers, getAllUsers,
+  registerUser, deleteUser, updateUser,
+ } from '../services/users.service';
 
 async function getById(req: Request, res: Response) {
-  const user: any = await getByIdUsers(req, res);
-  if (!user.err) {
-    res.status(200).json(user.data);
-  } else {
-    res.status(500).json(user.data);
-  }
+  await getByIdUsers(req)
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
 }
+
+async function getAll(req: Request, res: Response) {
+  await getAllUsers()
+    .then((data) => {
+      res.status(200).json(data);
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    });
+}
+
+async function update(req: Request, res: Response) {
+  await updateUser(req)
+    .then((data) => {
+      if (!data.err) {
+        res.status(200).json({message: data.data});
+      } else {
+        res.status(400).json({message: data.data});
+      }
+    })
+    .catch(() => {
+      res.status(500).json({messsage: 'Resource not updated'});
+    })
+}
+
+async function deleteById(req: Request, res: Response) {
+  await deleteUser(req)
+    .then((data) => {
+      res.status(200).json({message: data.data});
+    })
+    .catch(() => {
+      res.status(500).json({message: 'Resource not deleted'});
+    });
+}
+
+
+
+async function register(req: Request, res: Response) {
+  await registerUser(req)
+    .then((data) => {
+      if (!data.err) {
+        res.status(201).json(data.data);
+      } else {
+        res.status(400).json(data.data);
+      }
+    })
+    .catch((error) => {
+      res.status(500).json(error);
+    })
+}
+
+
 export {
-  getById,
+  getById, getAll,
+  register, deleteById,
+  update,
 }
--- server/src/services/users.service.ts
+++ server/src/services/users.service.ts
@@ -1,27 +1,121 @@
-import { Request, Response } from 'express';
+import { Request } from 'express';
 import Users from '../database/models/Users';
 
+const bcrypt = require('bcrypt');
 
-async function getByIdUsers(req: Request, res: Response) {
-  await Users.findAll({
+
+async function getByIdUsers(req: Request) {
+  const user = await Users.findAll({
     where: {
       id: req.params.id,
     },
-  })
-  .then((user: any) => {
+  });
+  return {
+    data: user,
+    err: false,
+  };
+}
+
+async function getAllUsers() {
+  const users: any = await Users.findAll()
+  return {
+    data: users,
+    err: false,
+  };
+}
+
+async function deleteUser(req: Request) {
+  await Users.delete({
+    where: {
+      id: req.params.id,
+    },
+  });
+  return {
+    data: 'Resource deleted successfully',
+    err: false,
+  };
+}
+
+async function updateUser(req: Request) {
+  let userNameExist: Boolean = false;
+  let emailExist: Boolean = false;
+
+  if (req.body.userName) {
+    const userName = Users.findOne({
+      where: {
+        userName: req.body.userName,
+      },
+    });
+    userName? userNameExist = true : userNameExist = false;
+  }
+  
+  if (req.body.email) {
+    const email = Users.findOne({
+      where: {
+        email: req.body.email,
+      },
+    });
+    email? emailExist = true : emailExist = false;
+  }
+  if (req.body.password) {
+    req.body.password = await bcrypt.hash(req.body.password, 13);
+  }
+  if (!emailExist && !userNameExist) {
+    await Users.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
     return {
-      data: user,
+      data: 'Resource updated',
       err: false,
-    }
-  })
-  .catch(() => {
+    };
+  } else if (emailExist) {
     return {
-      data: 'Erreur de Requete',
+      data: 'This email is already linked to an account',
       err: true,
     }
+  } else {
+    return {
+      data: 'This userName is already used',
+      err: true,
+    }
+  }
+}
+
+async function registerUser(req: Request) {
+  const userNameExist = Users.findOne({
+    where: {
+      userName: req.body.userName,
+    },
   });
+  const emailExists = Users.findOne({
+    where: {
+      email: req.body.email,
+    },
+  });
+  if (!userNameExist && !emailExists) {
+    req.body.password = await bcrypt.hash(req.body.password, 13);
+    await Users.create(req.body);
+    return {
+      data: 'User created succesfully',
+      err: false,
+    }
+  } else if (userNameExist) {
+    return {
+      data: 'This userName already exists',
+      err: true,
+    }
+  } else{
+    return {
+      data: 'This email is already linked to an account',
+      err: true,
+    }
+  }
 }
 
 export {
-  getByIdUsers,
+  getByIdUsers, getAllUsers,
+  registerUser, deleteUser,
+  updateUser,
 } 
