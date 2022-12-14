95fb40af96868dca876384aed908b8e7030e606a
moyon_e <moyon.enzo@icloud.com>
2022-12-14 16:45:34 +0100

    [FIX] Customer grid

--- client/src/pages/Customers.jsx
+++ client/src/pages/Customers.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import {DataManager, UrlAdaptor} from '@syncfusion/ej2-data';
 import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter } from '@syncfusion/ej2-react-grids';
 import { Header } from '../components';
 import { getAllUsers } from '../services/User.services';
@@ -8,31 +9,58 @@ const Customers = () => {
   const [users, setUsers] = useState(null);
 
   useEffect(() => {
+    refreshGrid()
+  }, [])
+
+  function refreshGrid() {
     getAllUsers()
-      .then((res) => {
-        if(res.status === 200) {
+      .then(
+        (res) => {
           setUsers(res.data);
         }
-      })
-  }, [])
+      );
+  }
+
+  function dataStateChange(args: any){
+    console.log('statechanged');
+    refreshGrid();
+  }
+
+  function dataSourceChanged(state: any) { 
+    debugger;
+    console.log(state)
+    if (state.action === "add") {
+      console.log('add');
+    } else if (state.action === "edit") {
+      console.log('edit');
+    } else if (state.requestType === "delete") {
+      console.log('delete');
+    }
+  }
 
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header title="Customers" />
       <GridComponent
         dataSource={users}
-        enableHover={false} allowPaging={true}
+        enableHover={true} allowPaging={true}
         pageSettings={{ pageCount: 5}}
-        persistSelection={true} toolbar={['Delete']}
-        allowDeleting={true} allowSorting={true} allowEditing={true}
+        persistSelection={true} toolbar={['Delete', 'Add', 'Edit', 'Search']}
+        editSettings={{allowDeleting: true, allowEditing: true, allowAdding: true}}
+        allowSorting={true}
+
+        dataSourceChanged={dataSourceChanged}
+        dataStateChange={dataStateChange}
       >
+
         <ColumnsDirective>
-          <ColumnDirective field='id' headerText='id' textAlign='left' width='50px'/>
-          <ColumnDirective field='userName' headerText='userName'/>
+          <ColumnDirective type='checkbox' width='40px'/>
+          {/*<ColumnDirective field='id' headerText='id' textAlign='left' width='70px' isPrimaryKey={true}/>*/}
+          <ColumnDirective field='userName'  headerText='userName' />
           <ColumnDirective field='firstName' headerText='Prénom'/>
           <ColumnDirective field='lastName' headerText='Nom'/>
           <ColumnDirective field='email' headerText='email'/>
-          <ColumnDirective field='admin' headerText='Administrateur'/>
+          <ColumnDirective type='boolean' displayAsCheckBox='true' editType="booleanedit" field='admin' headerText='Administrateur'/>
         </ColumnsDirective>
         <Inject services={[Page, Selection, Toolbar, Edit, Sort, Filter]} />
       </GridComponent>
