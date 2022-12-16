84e57dfbb15743de5a052c36e01f844a7e7cb9de
montei_j <montei_j@etna-alternance.net>
2022-12-16 12:32:17 +0100

    [DELETE] dotenv package wich was broken

--- client/src/services/ServiceHelper.js
+++ client/src/services/ServiceHelper.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 require('dotenv').config();
 
 export const api = axios.create({
-  baseURL: `http://localhost:${process.env.API_PORT}/`,
+  baseURL: `http://localhost:5000`,
   timeout: 10000,
   withCredentials: true,
   headers: {
