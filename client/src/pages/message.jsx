41b80b3a559701109682588bcdccc3ca6989ea98
montei_j <montei_j@etna-alternance.net>
2022-12-16 10:06:18 +0100

    [DELETE] code not used in message.jsx

--- client/src/pages/message.jsx
+++ client/src/pages/message.jsx
@@ -120,31 +120,6 @@ const Message = () => {
                 )
               }
             })}
-
-            {/* <div>
-              <p className='flex flex-row text-blue-600 font-bold'>
-                <img className='w-[20px] h-[20px] mr-2' src={userImage} alt="avatar" />
-                Les Autres
-              </p> 
-              <p className='bg-[#23f8a67c] ml-1 p-1 rounded-md overflow-hidden max-w-[50ch]'>
-                saluut ceci est un messgaejkbbbbbbbbaaaaaaaaaaa bbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbb bbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbb bbbbbbbbbbb bbbbbbbbbb bbbbb bbbbbbbbbbb
-              </p>
-            </div>
-            <div className='flex flex-row-reverse mr-1'>
-              <div>
-                <p className='flex flex-row-reverse text-pink-500 font-bold'>Nous</p>
-                
-                <p className=' p-1 bg-[#ffc0cbb5] text-black mr-2 rounded-md overflow-hidden max-w-[50ch]'>
-                  saluut ceci est un messgaejkbbbbbbbbaaaaaaaaaaa bbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbb bbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbb bbbbbbbbbbb bbbbbbbbbb bbbbb bbbbbbbbbbb
-                </p>
-              </div>
-            </div>
-            <div>
-              <p className='text-blue-600 font-bold'>Les Autres</p> 
-              <p className='bg-[#23f8a67c] ml-1 p-1 rounded-md overflow-hidden max-w-[50ch]'>
-                saluut ceci est un messgaejkbbbbbbbbaaaaaaaaaaa bbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbbbbbbbbbb bbbbbbbbbbbbbb bbbbbbbbbbbbbbbb bbbbbbbbbbbbbbbbbbb bbbbbbbbbbb bbbbbbbbbb bbbbb bbbbbbbbbbb
-              </p>
-            </div> */}
           </div>
         </div>
       </div>
